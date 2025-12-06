a3df0285b901e16d371a94a22d772b63af9b9126
Gabriel Moreno <gabriel.moreno@gmail.com>
2025-12-06 20:19:07 -0300

    Use VITE_API_URL instead of localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // URL do backend
+  baseURL: import.meta.env.VITE_API_URL, // agora usa variável da Vercel
 });
 
 export default api;
+
